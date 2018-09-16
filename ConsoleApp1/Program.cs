542d7a186d72fb046ef6490011bc1e5e75096865
Hasib <hasib.jhd@gmail.com>
2018-09-16 21:06:30 +0600

    input & Calculation in the same class

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please insert the first integer\n");
+            string S1 = Console.ReadLine();
+            int number1 = Convert.ToInt32(S1);
+
+
+            Console.WriteLine("Please insert the second integer\n");
+            string S2 = Console.ReadLine();
+            int number2 = Convert.ToInt32(S2);
+
+            int sum = Add(number1,number2);
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+
+        public static int Add(int first, int second)
+        {
+            return first + second;
+        }
+    }
+}
